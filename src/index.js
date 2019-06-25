7bd28c2428812da1b96eff82b648d14fc8202bd3
Brandon VanCamp <brandon@overtimewebdev.com>
2019-06-25 08:17:23 -0500

    Install dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+// const MongoStore = require('connect-mongo')(session);
+
+// mongodb connection
+mongoose.connect("mongodb://localhost:27017/");
+const db = mongoose.connection;
+// mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// use sessions for tracking logins
+// app.use(session({
+//   secret: 'treehouse loves you',
+//   resave: true,
+//   saveUninitialized: false,
+//   store: new MongoStore({mongooseConnection: db})
+// }));
 
 const app = express();
 
