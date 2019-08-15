e04db612f157b20ecc394505637c6ed0d93e4fcd
Brandon VanCamp <brandon@overtimewebdev.com>
2019-08-15 07:34:53 -0500

    n/a

--- src/index.js
+++ src/index.js
@@ -5,52 +5,50 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+// Create express app
+const app = express();
+const session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+const auth = require('basic-auth');
+const url = `mongodb://localhost:27017/course-api`;
 
-// DB Config
-const dbName = 'course-api';
-const url = "mongodb://localhost:27017/course-api";
-const mongoOptions = {useNewUrlParser : true};
+mongoose.connect(`${url}`);
+const db = mongoose.connection;
 
-// const MongoStore = require('connect-mongo')(session);
-
-// mongodb connection
-// mongoose.connect("mongodb://localhost:27017/course-api");
-// const db = mongoose.connection;
-
-
-// mongo error
-// db.on('error', console.error.bind(console, 'connection error:'));
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  client.close();
-
-  const db = client.db("course-api");
-
-  // console.log(db.collection('').find({}));
-
-  var cursor = db.collection('reviews').find({});
-  console.log(cursor, client);
-});
-
+// Mongo error catch
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // use sessions for tracking logins
-// app.use(session({
-//   secret: 'treehouse loves you',
-//   resave: true,
-//   saveUninitialized: false,
-//   store: new MongoStore({mongooseConnection: db})
-// }));
+app.use(session({
+  secret: 'course api secret',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({mongooseConnection: db})
+}));
 
-const app = express();
+// Make user ID available in template.
+app.use((req, res, next) => {
+  res.locals.currentUser = req.session.userId;
+  next();
+});
 
-// parse incoming requests
+// make user ID available in templates
+app.use((req, res, next) => {
+  res.locals.currentUser = req.session.userId;
+  next();
+});
+
+// MandoDB connection
+db.once("open", () => {
+  console.log("db connection successful");
+  // db.close();
+});
+
+// app.use(jsonCheck);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
-
+// app.use(jsonCheck);
 // serve static files from /public
 app.use(express.static(__dirname + '/public'));
 
@@ -59,7 +57,7 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
 // set our port
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
 // morgan gives us http request logging
 app.use(morgan('dev'));
@@ -67,28 +65,23 @@ app.use(morgan('dev'));
 // TODO add additional routes here
 
 const routes = require('./routes/index');
-app.use('/', routes);
-
+app.use('/api', routes);
 
 // uncomment this route in order to test the global error handler
-// app.get('/error', function (req, res) {
-//   throw new Error('Test error');
-// });
-
+// app.get('/error', function (req, res) {   throw new Error('Test error'); });
 // send 404 if no other route matched
 app.use((req, res) => {
-  res.status(404).json({
-    message: 'Route Not Found'
-  })
+  res
+    .status(404)
+    .json({message: 'Route Not Found'})
 })
 
 // global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(err.status || 500).json({
-    message: err.message,
-    error: {}
-  });
+  res
+    .status(err.status || 500)
+    .json({message: err.message, error: {}});
 });
 
 // start listening on our port
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,184 @@
+'use strict';
+
 const express = require('express');
 const router = express.Router();
+const Reviews = require('../models/models').Reviews;
+const Users = require('../models/user').Users;
+const Courses = require('../models/course').Courses;
+// const mid = require('../middleware/mid').loggedOut;
+const mid = require('../middleware/mid');
+
+const {check, validationResult} = require('express-validator/check');
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
+
+// Route that returns the current authenticated user.
+router.get('/users', mid.authenticateUser, (req, res, next) => {
+  const user = req.currentUser;
+  
+  // res.json({name: user.fullName, username: user.emailAddress, password: user.password});
+});
+
+// Route that creates a new user.
+router.post('/users', [
+  check('fullName')
+    .exists({checkNull: true, checkFalsy: true})
+    .withMessage('Please provide a value for "fullName"'),
+  check('emailAddress')
+    .exists({checkNull: true, checkFalsy: true})
+    .withMessage('Please provide a value for "emailAddress"'),
+  check('password')
+    .exists({checkNull: true, checkFalsy: true})
+    .withMessage('Please provide a value for "password"')
+], (req, res, next) => {
+  if(req.session) {
+    //Delete session object
+    req.session.destroy((err) => {
+      if(err) {
+        return next(err);
+      }
+       else {
+        return console.log('Session destroyed : true.');
+      }
+    });
+  }
+
+  // Password match check.
+  if (req.body.password !== req.body.confirmPassword) {
+    const err = new Error('Passwords do not match.');
+    err.status = 400;
+    return next(err);
+  }
+
+  // Attempt to get the validation result from the Request object.
+  const errors = validationResult(req);
+
+  // If there are validation errors...
+  if (!errors.isEmpty()) {
+    // Use the Array `map()` method to get a list of error messages.
+    const errorMessages = errors
+      .array()
+      .map(error => error.msg);
+    // Return the validation errors to the client.
+    return res
+      .status(400)
+      .json({errors: errorMessages});
+  }
+
+  // Get the user from the request body.
+  const user = new Users(req.body);
+
+  // Hash the new user's password.
+  user.password = bcryptjs.hashSync(user.password);
+
+  // Save new user to database.
+  Users.create(user, (err, user) => {
+  // Set the status to 201 Created and end the response.  
+    if (err) {
+      return next(err);
+    } else {
+      req.session.userId = user._id;
+      return res.status(201);
+    }
+  });
+});
+
+//  Course routes.
+router.get('/courses', (req, res, next) => {
+  Courses
+    .find({})
+    .sort({createdAt: -1})
+    .exec(function (err, courses) {
+      if (err) 
+        return next(err);
+      res.json(courses);
+    });
+});
+
+// Get specific course.
+router.get('/courses/:courseId', (req, res, next) => {
+  Courses.findOne({_id: req.params.courseId})
+  // .sort({createdAt: -1})
+    .exec(function (err, courses) {
+      // if(err) return next(err);
+      res.json(courses);
+    });
+});
+
+//Update course.
+router.put('/courses/:coursesId', (req, res, next) => {
+  // Create new course Add data to DB. Return no data && set params to '/'.
+  Courses
+    .findById({_id: req.params.coursesId})
+    .exec((err, course) => {
+      if(err) return next(err);
+      if (!course) {
+        const error = new Error('Can not find course ID, please search a valid ID.');
+        error.status = 404;
+        return next(error);
+      } 
+
+      res.json(course);
+    });
+});
+
+// Create reviews for specific course.
+router.post('/courses/:courseId/reviews', (req, res, next) => {
+  // Create new course Add data to DB. Return no data && set params to '/'.
+});
+
+// Create new course.
+router.post('/courses', [
+  check('title')
+    .exists({checkNull: true, checkFalsy: true})
+    .withMessage('Please provide a value for "title"'),
+  check('description')
+    .exists({checkNull: true, checkFalsy: true})
+    .withMessage('Please provide a value for "description"'),
+  check('steps')
+    .exists({checkNull: true, checkFalsy: true})
+    .withMessage('Please provide a value for "steps"'),
+], (req, res, next) => {
+  // Attempt to get the validation result from the Request object.
+  const errors = validationResult(req);
+  
+  // Check for authorization.
+  if(!req.session.userId) {
+    const err = new Error('You are unauthorized to view this page.');
+    err.status = 403;
+    return next(err);
+  }
+  Users
+    .findById(req.session.userId)
+    .exec( (error, user) => {
+      // res.json(user, req.session.userId);
+      console.log(user, 'posted to db', req.session.userId);
+    });
+
+  // If there are validation errors...
+  if (!errors.isEmpty()) {
+    // Use the Array `map()` method to get a list of error messages.
+    const errorMessages = errors
+      .array()
+      .map(error => error.msg);
+
+    // Return the validation errors to the client.
+    return res
+      .status(400)
+      .json({errors: errorMessages});
+  }
+
+  // Save new course data
+  const course = new Courses(req.body);
+  console.log(course);
+  // course.save((err, course) => {
+    // if (err) 
+      // return next(err);
+    res.status(201);
+    res.json(course);
+  // });
+});
 
 
-// send a friendly greeting for the root route
-router.get('/', (req, res) => {
-   res.json({
-     message: 'Welcome to the Course Review API'
-   });
- });
 
- 
-// router.get('/', function (req, res, next) {
-//   return res.render('index', {message: 'Welcome to the Course Review API'});
-// });
-
-
-
-module.exports = router;
+            module.exports = router;
